b8075aa49df32e7c5b6a50e09a60a82a4d2fdb03
Tom Lieber <tom@alltom.com>
2009-12-31 00:15:12 -0500

    spacing was bothering me

--- ckv.h
+++ ckv.h
@@ -12,10 +12,10 @@ typedef struct VM *VMPtr;
 int open_ckvbaselite(lua_State *L);
 int open_ckvugen(lua_State *L);
 
-typedef void (*AudioCallback)( double *outputBuffer, double *inputBuffer,
-                               unsigned int nFrames,
-                               double streamTime,
-                               void *userData );
+typedef void (*AudioCallback)(double *outputBuffer, double *inputBuffer,
+                              unsigned int nFrames,
+                              double streamTime,
+                              void *userData);
 int start_audio(AudioCallback callback, int sample_rate, void *data);
 void stop_audio(void);
 
