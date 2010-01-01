c092bb9beca4425f72c134ab640ecee799748d16
Tom Lieber <tom@alltom.com>
2010-01-01 15:33:46 -0500

    dac = speaker

--- ugen/ugen.c
+++ ugen/ugen.c
@@ -127,7 +127,9 @@ int open_ckvugen(lua_State *L) {
 	lua_getfield(L, -1, "new");
 	lua_pushvalue(L, -2); /* push Gain again (argument to new) */
 	lua_call(L, 1, 1);
-	lua_setglobal(L, "dac");
+	lua_pushvalue(L, -1); /* dup dac */
+	lua_setglobal(L, "dac"); /* pops one */
+	lua_setglobal(L, "speaker"); /* pops other */
 	lua_pop(L, 1); /* pop Gain */
 	
 	return 1; /* return globals */
