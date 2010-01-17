a6340fc7d93a3ccb72137fe3f943498d56dad1a5
Tom Lieber <tom@alltom.com>
2010-01-17 14:09:22 -0800

    bug in Follower

--- ugen/follower.c
+++ ugen/follower.c
@@ -12,6 +12,7 @@ open_ugen_follower(lua_State *L)
 	" new = function(class, half_life)"
 	"  return {"
 	"   decay = math.exp(math.log(0.5) / (half_life or (sample_rate / 16.))),"
+	"   last = 0.0,"
 	"   tick = function(self)"
 	"    local in_sample = math.abs(UGen.sum_inputs(self));"
 	"    if in_sample > self.last then"
